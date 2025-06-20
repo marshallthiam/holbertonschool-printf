0c3682ed2990b4635060f296c4c06806cbabf32f
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-20 23:14:14 +0000

    main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Test integer: %d\n", 123);
+    _printf("Negative: %i\n", -456);
+    _printf("Zero: %d\n", 0);
+    _printf("Mixed: %s %c %d %%\n", "Score", 'A', 100);
+    return (0);
+}
