707add9c0547db55408b25a6c6281946bafb3d59
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-20 23:21:30 +0000

    main.c

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int print_number(int n);
+
+#endif
